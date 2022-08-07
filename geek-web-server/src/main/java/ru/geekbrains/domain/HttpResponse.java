80b1f7e74f865af56ff0785453e234060bca4a71
Andrey Bezrukov <andruxa.ic@gmail.com>
2022-08-07 20:08:34 +0400

    ДЗ 2

--- geek-web-server/src/main/java/ru/geekbrains/domain/HttpResponse.java
+++ geek-web-server/src/main/java/ru/geekbrains/domain/HttpResponse.java
@@ -1,16 +1,34 @@
 package ru.geekbrains.domain;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class HttpResponse {
 
     private int statusCode;
 
+    private String statusText;
+
     private Map<String, String> headers;
 
     private String body;
 
     public HttpResponse() {
+    }
+
+    public HttpResponse(int statusCode, String statusText, String body) {
+        this.statusCode = statusCode;
+        this.statusText = statusText;
+        this.body = body;
+        this.headers = new HashMap<>();
+        headers.put("Content-Type", "text/html; charset=utf-8");
+    }
+
+    public HttpResponse(int statusCode, String statusText, Map<String, String> headers, String body) {
+        this.statusCode = statusCode;
+        this.statusText = statusText;
+        this.headers = headers;
+        this.body = body;
     }
 
     public int getStatusCode() {
@@ -35,5 +53,13 @@ public class HttpResponse {
 
     public void setBody(String body) {
         this.body = body;
+    }
+
+    public String getStatusText() {
+        return statusText;
+    }
+
+    public void setStatusText(String statusText) {
+        this.statusText = statusText;
     }
 }
